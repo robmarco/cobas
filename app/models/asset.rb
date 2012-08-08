dd7ea6b3289491a032d80c50a7dd3877db217188
Roberto Marco <robmarco@gmail.com>
2012-08-08 09:18:10 +0100

    Delete files

--- app/models/asset.rb
+++
@@ -1,7 +0,0 @@
-class Asset < ActiveRecord::Base
-  attr_accessible :asset_content_type, :asset_file_name, :asset_file_size, :asset_updated_at, :respond_id, :asset
-
-  belongs_to :respond
-  
-  has_attached_file :asset
-end
